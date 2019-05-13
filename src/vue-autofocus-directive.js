030a86fa7de57b9cbc07fa18e3fb1ec37db6d916
Ivan Demidov <Scrum@list.ru>
2019-05-13 18:25:56 +0300

    feat: add modifier byAttribute for dynamic attribute autofocuse

--- src/vue-autofocus-directive.js
+++ src/vue-autofocus-directive.js
@@ -1,9 +1,18 @@
 // Register a global custom directive called `v-focus`
 const directive = {
   // When the bound element is inserted into the DOM...
-  inserted: function(el) {
+  inserted(el, binding) {
     // Focus the element
-    el.focus();
+    const onFocus = () => el.focus();
+    // If directive has modifier `byAttribute`
+    if (binding.modifiers.byAttribute) {
+      // If element has attribute autofocus
+      if (el.getAttribute('autofocus')) {
+        onFocus();
+      }
+    } else {
+      onFocus();
+    }
   }
 };
 
