74601a2f6ca540e7fe2c29a5e44ef0d2d3020e82
Ivan Demidov <Scrum@list.ru>
2019-05-14 14:47:22 +0300

    perf: change modifier to value

--- src/vue-autofocus-directive.js
+++ src/vue-autofocus-directive.js
@@ -2,17 +2,13 @@
 const directive = {
   // When the bound element is inserted into the DOM...
   inserted(el, binding) {
-    // Focus the element
-    const onFocus = () => el.focus();
-    // If directive has modifier `byAttribute`
-    if (binding.modifiers.byAttribute) {
-      // If element has attribute autofocus
-      if (el.getAttribute('autofocus')) {
-        onFocus();
-      }
-    } else {
-      onFocus();
+    // If directive has value
+    if (binding.value !== undefined && !binding.value) {
+      return;
     }
+
+    // Focus the element
+    el.focus();
   }
 };
 
