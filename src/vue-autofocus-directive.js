e0dc3b6b4a7a777f811c327f491fe576e875a931
Dominik Pschenitschni <dpschen@users.noreply.github.com>
2020-07-21 14:38:49 +0200

    Focus element on activated hook call of the vnode.

--- src/vue-autofocus-directive.js
+++ src/vue-autofocus-directive.js
@@ -1,14 +1,17 @@
-// Register a global custom directive called `v-focus`
-const directive = {
-  // When the bound element is inserted into the DOM...
-  inserted(el, binding) {
-    // If directive has bound value
-    if (binding.value !== undefined && !binding.value) {
-      return;
-    }
-    // Focus the element
-    el.focus();
-  }
-};
+function focusElement(el, binding) {
+  // If directive has bound value
+  if (binding.value !== undefined && !binding.value) return;
 
-export default directive;
+  // Focus the element
+  el.focus();
+}
+
+// Register a global custom directive called `v-focus`
+export default {
+  bind(el, binding, vnode) {
+    // When the component of the element gets activated
+    vnode.context.$on('hook:activated', () => focusElement(el, binding));
+  },
+  // When the bound element is inserted into the DOM...
+  inserted: focusElement,
+};
